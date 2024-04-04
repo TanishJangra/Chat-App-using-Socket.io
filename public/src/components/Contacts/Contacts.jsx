e1d6fefdc9c39de6e30a5f1decaa808f2c93085c
TanishJangra <96981050+TanishJangra@users.noreply.github.com>
2024-04-04 20:09:44 +0530

    setup Chat ui containing all users in contact

--- public/src/components/Contacts/Contacts.jsx
+++ public/src/components/Contacts/Contacts.jsx
@@ -1,16 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Contacts.css";
+import img from "./../../assets/meetme.png";
+
 const Contacts = ({ contacts, currentUser }) => {
   console.log("contacts details are: ", contacts);
+
+  const [currentUsername, setCurrentUsername] = useState(undefined);
+  const [currentUserImage, setCurrentUserImage] = useState(undefined);
+  const [currentSelected, setCurrentSelected] = useState(undefined);
+
+  useEffect(() => {
+    if (currentUser) {
+      setCurrentUserImage(currentUser.avatarImage);
+      setCurrentUsername(currentUser.username);
+    }
+  }, [currentUser]);
+  console.log("avatarImage is:::::::::::", currentUserImage);
+  const changeCurrentChat = (ind, contact) => {};
+
   return (
-    <div className="contactsContainer">
-      {contacts.map((item, ind) => (
-        <div key={ind} className="contactsDiv">
-          <p className="contactItemAvatarImg">{item.avatarImage}</p>
-          <p className="contactItemUsername">{item.username}</p>
+    <>
+      {currentUserImage && currentUsername && (
+        <div className="contactsContainer">
+          <div className="contactHeader">
+            <img src={img} alt="img" className="logoImg" />
+            <h1>ChatBot</h1>
+          </div>
+          <div className="chatsContact">
+            {contacts.map((item, ind) => (
+              <div
+                key={ind}
+                className={`contactsDiv ${
+                  ind === currentSelected ? "selected" : ""
+                }`}
+              >
+                <img
+                  src={`data:image/svg+xml;utf8,${encodeURIComponent(
+                    item.avatarImage
+                  )}`}
+                  alt="img"
+                  className="usersImg"
+                />
+                <p className="contactItemUsername">{item.username}</p>
+              </div>
+            ))}
+          </div>
+          <div className="current-user">
+            <div className="avatar">
+              <img
+                src={`data:image/svg+xml;utf8,${encodeURIComponent(
+                  currentUserImage
+                )}`}
+                alt="img"
+                className="usersImg"
+              />
+            </div>
+            <div className="username">
+              <h2>{currentUsername}</h2>
+            </div>
+          </div>
         </div>
-      ))}
-    </div>
+      )}
+    </>
   );
 };
 
